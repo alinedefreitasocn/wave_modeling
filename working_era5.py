c9a5b75b375c81d7e136b9b3547048a3bb40ed0c
Aline <alfreitas.ocn@gmail.com>
2020-09-22 10:10:59 +0100

    Atualizaçoes de 22 de setembro

--- working_era5.py
+++ working_era5.py
@@ -27,7 +27,7 @@ from cfgrib import xarray_store
 
 
 # single file
-dataDIR = '/home/aline/Documents/ERA5/montly_mean_1990_2020.grib'
+dataDIR = '/home/aline/Documents/IST_investigation/ERA5/montly_mean_1990_2020.grib'
 
 DS = cfgrib.open_datasets(dataDIR)[0]
 
@@ -41,3 +41,24 @@ ax = plt.axes(projection=ccrs.PlateCarree())
 ax.coastlines()
 DS.isel(time=0).swh.plot()
 plt.show(block=False)
+
+
+for t in DS.time.values:
+
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.coastlines()
+    DS.sel(time=str(t)).swh.plot(
+        # figsize = (12,6),      # We'll make it a bit bigger
+        vmin=0, vmax=7
+    )
+    plt.title("Time = " + str(t))
+    plt.savefig(f"/home/aline/Documents/IST_investigation/ERA5/images/Python_Animation/Python_Animation_01_frame_{str(t)}.png")
+    plt.close()
+
+
+# Fazer as animacoes. Essa parte entra na linha de
+# comando fora do ambiente python
+!ls /home/aline/Documents/ERA5/images/Python_Animation/
+
+!convert /home/aline/Documents/ERA5/images/Python_Animation/Python_Animation_01_frame*png /home/aline/Documents/ERA5/images/Python_Animation/Python_Animation_01.gif
+display(HTML("<img src='../images/Python_Animation/Python_Animation_01.gif' />"))
