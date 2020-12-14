7176c038c50bfe38ae454e7c830b5c307df9f643
Aline <alfreitas.ocn@gmail.com>
2020-12-14 20:39:04 +0000

    atualizacoes da noite. plot das figuras esta atualizado e funcionando semi automatico para cada parametro de onda

--- correlation_index_AO.py
+++ correlation_index_AO.py
@@ -4,9 +4,11 @@ from cfgrib import xarray_store
 import cfgrib
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
 import cartopy.crs as ccrs
 import numpy as np
 from faz_figuras import *
+
 
 findex = '/home/aline/Documents/Dados/ERA5/index_era5.nc'
 fwave = '/home/aline/Documents/Dados/ERA5/montly_mean_1990_2020.grib'
@@ -17,45 +19,69 @@ index = xr.open_dataset(findex)
 index=index['__xarray_dataarray_variable__'].rename('indices')
 index_crop = index.sel(time=slice(dwave.isel(time=0).time.values,
                                 dwave.isel(time=-1).time.values))
-
-correlacao_hs = xr.corr(dwave.swh, index_crop, dim='time')
-correlacao_tp = xr.corr(dwave.mwp, index_crop, dim='time')
-correlacao_dir = xr.corr(dwave.mwd, index_crop, dim='time')
+# Compute the Pearson correlation coefficient between
+# two DataArray objects along a shared dimension
+correlacao = {'Hs': xr.corr(dwave.swh, index_crop, dim='time'),
+              'Tp': xr.corr(dwave.mwp, index_crop, dim='time'),
+              'Wave Direction': xr.corr(dwave.mwd, index_crop, dim='time')}
+# correlacao = xr.Dataset(correlacao)
 
 
 limites = [-104, 40, 20, 85]
-clevs = np.linspace(-1, 1, 10)
-proj = ccrs.LambertConformal(central_longitude=-40, central_latitude=50, \
-                                 standard_parallels=(20,30))
+#clevs = np.linspace(-1, 1, 10)
+clevs = np.arange(-1, 1.2, 0.2)
+colormap = plt.cm.Spectral
+proj = ccrs.LambertConformal(central_longitude=-40,
+                             central_latitude=50,
+                             standard_parallels=(20,30))
 #proj= ccrs.Orthographic(0, 90)
-faz_mapa_corr(proj, clevs, coords_lim=limites)
-correlacao_hs.plot.contourf(levels=clevs,
-                            cmap=plt.cm.BrBG,
-                         transform=ccrs.PlateCarree(),
-                         add_colorbar=True
-                         )
 
-plt.title('Hs/AO correlation')
-plt.show(block=False)
 
-faz_mapa_corr(proj, clevs, coords_lim=limites)
-correlacao_hs.plot.contourf(levels=clevs,
-                            cmap=plt.cm.BrBG,
-                         transform=ccrs.PlateCarree(),
-                         add_colorbar=True
-                         )
+for k in correlacao.keys():
+    fig, ax = faz_mapa_corr(proj,
+                            clevs,
+                            coords_lim = limites)
+    correlacao[k].plot.contourf(levels=clevs,
+                                cmap=colormap,
+                                transform=ccrs.PlateCarree(),
+                                add_colorbar=True
+                                )
 
-plt.title('Hs/AO correlation')
-plt.show(block=False)
+    plt.title(k + '/AO correlation')
+    plt.show(block=False)
+    fig.savefig('/home/aline/Dropbox/IST_investigation/Teleconnections' +
+                '/correlacoes/'+ k + '_ao_NA.png')
+    plt.close()
 
-correlacao_tp.plot.contourf(levels=clevs,
-                            cmap=plt.cm.BrBG,
-                         transform=ccrs.PlateCarree(),
-                         add_colorbar=True
-                         )
-plt.title('Tp/AO correlation')
-plt.show(block=False)
+" Selecionando o periodo de inverno "
+index_winter = index_crop.sel(time=index_crop['time.season']=='DJF')
+dwave_winter = dwave.sel(time=dwave['time.season']=='DJF')
+correlacao_winter = {'Hs': xr.corr(dwave_winter.swh,
+                                    index_winter, dim='time'),
+              'Tp': xr.corr(dwave_winter.mwp, index_winter, dim='time'),
+              'Wave Direction': xr.corr(dwave_winter.mwd,
+                                        index_winter, dim='time')}
+#correlacao_winter = xr.Dataset(correlacao_winter)
 
+for k in correlacao_winter.keys():
+    fig, ax = faz_mapa_corr(proj,
+                            clevs,
+                            coords_lim = limites)
+    correlacao_winter[k].plot.contourf(levels=clevs,
+                                cmap=colormap,
+                                transform=ccrs.PlateCarree(),
+                                add_colorbar=True
+                                )
+
+    plt.title(k + '/AO correlation - Winter (DJF)')
+    plt.show(block=False)
+    fig.savefig('/home/aline/Dropbox/IST_investigation/Teleconnections' +
+                '/correlacoes/'+ k + '_ao_NA_winter_DJF.png')
+    plt.close()
+
+
+
+###############################################################################
 
 plt.figure()
 ax = plt.subplot(projection=ccrs.Orthographic(0, 90))
--- modules/faz_figuras.py
+++ modules/faz_figuras.py
@@ -1,10 +1,23 @@
-def faz_mapa_corr(proj, levels, coords_lim=None):
+import matplotlib
+import matplotlib.pyplot as plt
 
-    fig, ax = plt.subplots(subplot_kw={'projection': proj})
+font = {'family' : 'normal',
+        'weight' : 'normal',
+        'size'   : 22}
+
+matplotlib.rc('font', **font)
+
+
+def faz_mapa_corr(proj, levels, coords_lim=None):
+    matplotlib.rcParams['contour.negative_linestyle'] = 'solid'
+
+    fig, ax = plt.subplots(figsize=(20, 15),
+                            dpi=400,
+                            subplot_kw={'projection': proj})
     ax.coastlines()
     ax.set_global()
     if coords_lim != None:
         print('Alterando limites do mapa!')
         ax.set_extent(coords_lim, ccrs.PlateCarree())
 
-    return ax, fig
+    return fig, ax
